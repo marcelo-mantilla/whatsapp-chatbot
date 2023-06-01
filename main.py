5a89fe13dcdfc1d29b20590fce1b52b1f5e66695
marcelomantilla <marcelo.mantilla@appcabinet.com>
2023-06-01 12:07:13 +0200

    initialize postgres database

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from flask import Flask
-from whatsapp.router import whatsapp
+from flask_migrate import Migrate
+from api.status.router import status_router
+from api.users.router import users_router
+from api.whatsapp.router import whatsapp_router
+from db import db
+
+import os
 
 
-app = Flask(__name__)
-app.register_blueprint(whatsapp)
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    app.register_blueprint(status_router)
+    app.register_blueprint(users_router)
+    app.register_blueprint(whatsapp_router)
+
+    return app
 
 
-app.run(port=5000)
+create_app().run(port=5000)
